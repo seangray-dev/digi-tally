9775a7501029e80d042e97853a5ac67e40e12cdf
Sean Gray <seangray.dev@gmail.com>
2024-04-06 16:39:16 -0400

    implement contract

--- contracts/countercontract/contracts/Contract.sol
+++ contracts/countercontract/contracts/Contract.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
-    constructor() {}
+    uint256 public count = 0;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decremt() public {
+        require(count > 0, "count must be greater than 0");
+        count -= 1;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
 }
